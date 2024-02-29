954797cfca8b168602b9c83ef7dac4d1982a60dd
soheib <soheibkenden@gmail.com>
2024-02-29 08:16:54 +0100

    commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -24,7 +24,7 @@ const Experience = () => {
             </section>
 
             <div className="pb-10">
-                <a href="https://drive.google.com/file/d/1LOyZuUD7uHdTCp8NLcifnpDgatU3VR58/view?usp=sharing" target="_blank" rel="noreferrer" className="group pb-11 text-slate-200 font-semibold tracking-wide duration-200 hover:text-[#64ffda]">
+                <a href="https://drive.google.com/file/d/1335vXpysrPShWjuCLb2FHjqfU__jRMFU/view?usp=sharing" target="_blank" rel="noreferrer" className="group pb-11 text-slate-200 font-semibold tracking-wide duration-200 hover:text-[#64ffda]">
                     <span>
                         View Full Resume
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className=" inline h-4 w-4 transition-transform group-hover:-translate-y-1 group-hover:translate-x-1 motion-reduce:transition-none ml-1 " aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path></svg>
